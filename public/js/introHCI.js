d33c4af56e10caa5e1ee02cb867f8713d144b258
marbely <marbely13@gmail.com>
2015-02-13 05:20:32 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
+	
+
 
 	$('#colorBtn').click(randomizeColors);
 }
@@ -18,6 +20,7 @@ function initializePage() {
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
+
 	// Prevent following the link
 	e.preventDefault();
 
@@ -27,12 +30,45 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, getData)
 }
 
+    
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+		$.get("/palette", function(result) {
+		console.log(result);
+		var colors = result.colors.hex;
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
+
+
+
+
+
+
+} 
+function getData(result){
+	console.log(result);
+  var projectHTML = '<a href="#" class="thumbnail">' +
+'   <img src=" ' + result['image'] + '" class="img">' +
+'   <p>' + result['title'] + '</p>' +
+' <p><small>' + result['date'] + '</small></p></a>' +
+'   <p>' + result['summary'] + '</p>';
+
+	//$("#project-container").html(projectHTML);
+	//$("project-description").html(result['summary']);
+	$('#project'+result['id']+' .details').html(projectHTML);
+
+
 }
